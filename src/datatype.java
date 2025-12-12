764ccbf6ec58428d1541a1800588ca4f1f90dcfc
Amit Rathod <amitsrathod1011@gmail.com>
2025-12-12 15:40:46 +0530

    first commit

---
+++ src/datatype.java
@@ -0,0 +1,23 @@
+
+public class datatype {
+	public static void main(String[] args) {
+		
+		System.out.println("Byte size is="+Byte.SIZE+"byte");
+		System.out.println("Short size is="+Short.SIZE+"byte");
+		System.out.println("Interger size is="+Integer.SIZE+"byte");
+		System.out.println("Float size is="+Float.SIZE+"byte");
+		System.out.println("Long size is="+Long.SIZE+"byte");
+		System.out.println("Double size is="+Double.SIZE+"byte");
+		System.out.println("Character size is="+Character.SIZE+"byte");
+		System.out.println("Boolean is not defined");
+		
+		System.out.println("Byte BYTES is="+Byte.BYTES+"byte");
+		System.out.println("Short BYTES is="+Short.BYTES+"byte");
+		System.out.println("Interger BYTES is="+Integer.BYTES+"byte");
+		System.out.println("Float BYTES is="+Float.BYTES+"byte");
+		System.out.println("Long BYTES is="+Long.BYTES+"byte");
+		System.out.println("Double BYTES is="+Double.BYTES+"byte");
+		System.out.println("Character BYTES is="+Character.BYTES+"byte");
+		System.out.println("Boolean is not defined");
+	}
+}
