81af199e9f9623601ab074cc75753659020a8e42
Amit Rathod <amitsrathod1011@gmail.com>
2025-12-12 22:23:24 +0530

    Update datatype.java

--- src/datatype.java
+++ src/datatype.java
@@ -19,5 +19,14 @@ public class datatype {
 		System.out.println("Double BYTES is="+Double.BYTES+"byte");
 		System.out.println("Character BYTES is="+Character.BYTES+"byte");
 		System.out.println("Boolean is not defined");
+
+		System.out.println(Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+		System.out.println(Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+		System.out.println(Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+		System.out.println((int) Character.MIN_VALUE + " to " + (int) Character.MAX_VALUE);
+		System.out.println("Boolean Not Found");
 	}
 }
